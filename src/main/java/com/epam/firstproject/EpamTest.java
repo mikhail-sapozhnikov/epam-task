2fc27e1664040a7eca38e09ce7e8208c6d114c7e
Mikhail Sapozhnikov <mikhail_sapozhnikov@epam.com>
2016-11-20 10:23:42 +0300

    Third commit

--- src/main/java/com/epam/firstproject/EpamTest.java
+++ src/main/java/com/epam/firstproject/EpamTest.java
@@ -21,32 +21,40 @@ public class EpamTest {
         baseUrl = "https://www.epam.com/";
         driver.manage().timeouts().implicitlyWait(360, TimeUnit.SECONDS);
         driver.get(baseUrl);
-
     }
 
-//    @DataProvider(name = "test1")
-//    public Object[][] createData1() {
-//        return new Object[][] {
-//                { "Cedric", new Integer(36) },
-//                { "Anne", new Integer(37)},
-//        };
-//    }
+    @DataProvider(name = "Test data")
+    public Object[][] createData() {
+        return new Object[][]{
+                {"Core Engineering", "HomeSolutionsCore Engineering"},
+                {"Product Development", "HomeSolutionsCore EngineeringProduct Development"},
+                {"Engineering Excellence", "HomeSolutionsCore EngineeringEngineering Excellence"},
+                {"Core Technologies", "HomeSolutionsCore EngineeringCore Technologies"},
+                {"Assurance", "HomeSolutionsCore EngineeringAssurance"},
+                {"Advanced Technology", "HomeSolutionsAdvanced Technology"},
+                {"Next-Gen Architecture", "HomeSolutionsAdvanced TechnologyNext-Gen Architecture"},
+                {"Agility", "HomeSolutionsAdvanced TechnologyAgility"},
+                {"Internet of Things", "HomeSolutionsAdvanced TechnologyInternet of Things"},
+                {"R&D", "HomeSolutionsAdvanced TechnologyR&D"},
+                {"Intelligent Enterprise", "HomeSolutionsIntelligent Enterprise"},
+                {"Enterprise Applications", "HomeSolutionsIntelligent EnterpriseEnterprise Applications"},
+                {"Data & Analytics", "HomeSolutionsIntelligent EnterpriseData & Analytics"},
+                {"Application & Cloud Management", "HomeSolutionsIntelligent EnterpriseApplication & Cloud Management"},
+                {"Collaboration/BPM", "HomeSolutionsIntelligent EnterpriseCollaboration/BPM"},
+                {"Digital Engagement", "HomeSolutionsDigital Engagement"},
+                {"Strategy & Experience", "HomeSolutionsDigital EngagementStrategy & Experience"},
+                {"Digital Marketing", "HomeSolutionsDigital EngagementDigital Marketing"},
+                {"Commerce", "HomeSolutionsDigital EngagementCommerce"},
+                {"Mobility", "HomeSolutionsDigital EngagementMobility"}
+        };
+    }
 
 
-    @Test//(dataProvider = "test1")
-    public void testCoreEngineeringLink() throws InterruptedException {
-        System.out.println("Begin test 1");
-
+    @Test(dataProvider = "Test data")
+    public void testCoreEngineeringLink(String menuText, String crumbsExpected) throws InterruptedException {
         EpamHomePage page = new EpamHomePage(driver);
-        page.pointCursor(driver);
-        page.menuCoreEngineering.click();
-
-
-
+        page.checkMenuItem(driver, menuText, crumbsExpected);
 
     }
-
-
-
 
 }
--- src/main/java/com/epam/pom/EpamHomePage.java
+++ src/main/java/com/epam/pom/EpamHomePage.java
@@ -1,10 +1,14 @@
 package com.epam.pom;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
+import java.util.List;
 
 
 public class EpamHomePage {
@@ -13,16 +17,16 @@ public class EpamHomePage {
         PageFactory.initElements(driver, this);
     }
 
-    public void pointCursor(WebDriver driver) {
+    // Point cursor to SOLUTIONS menu item
 
+    private void pointCursor(WebDriver driver) {
         Actions action = new Actions(driver);
-        action.moveToElement(menuSolutions);
+        action.moveToElement(driver.findElement(By.linkText("SOLUTIONS")));
         action.perform();
         this.sleep(2);
     }
 
-    public void sleep(int seconds)
-    {
+    private void sleep(int seconds) {
         try {
             Thread.sleep(seconds * 1000);
         } catch (InterruptedException e) {
@@ -30,15 +34,25 @@ public class EpamHomePage {
         }
     }
 
-    @FindBy(linkText = "SOLUTIONS")
-    public WebElement menuSolutions;
-
-    @FindBy(linkText = "Core Engineering")
-    public WebElement menuCoreEngineering;
+    // Return page crumbs as a text
 
     @FindBy(className = "header-menu-crumb-item")
-    public WebElement breadCrumbs;
+    private List<WebElement> crumbsList;
 
-//    findBy("//ol[@class='breadcrumb']").getText().equals("First / Second / Third");Home Solutions Core Engineering
+    private String crumbsText() {
+        String s1 = "";
+        for (WebElement we : crumbsList) {
+            s1 = s1 + we.getText();
+        }
+        return s1;
+    }
+
+    // Verify that page crumbs are correct
+
+    public void checkMenuItem(WebDriver driver, String menuText, String crumbsExpected) {
+        pointCursor(driver);
+        driver.findElement(By.linkText(menuText)).click();
+        Assert.assertEquals(crumbsText(), crumbsExpected, "Wrong crumbs!");
+    }
 
 }
